88dd0d2bb04d26224b3bab5b1f691ca5fc14e1ac
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-13 16:23:40 +0100

    Commit jusqu'à Exercice 2.2.2

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,37 @@
+fn average(x: f64, y: f64) -> f64 {
+    let result = (x+y)/2.0;
+    return result;
+}
+
+struct Rectangle{
+    length: f64,
+    width: f64,
+}
+
+fn perimetre(rectangle: Rectangle) -> f64{
+    let result = (rectangle.length*2.0+rectangle.width*2.0);
+    return result;
+}
+
+fn perimetre2(rectangle: &Rectangle) ->f64{
+    let result = (rectangle.length*2.0+rectangle.width*2.0);
+    return result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Exercice 2.1
+    let x = 13.0;
+    let y = 12.0;
+    println!("{}", average(x, y));
+    println!("{}", average(x, y));
+
+    //Exercice 2.2.1
+    let my_rectangle = Rectangle{
+        length : 2.0,
+        width : 2.0,
+    };
+    println!("{}", perimetre2(&my_rectangle)); //Changer pour l'exercice 2.2.2
+
+    //Exercice 2.2.2
+    println!("{}", perimetre2(&my_rectangle));
 }
