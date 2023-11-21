4b1205a359801c8978d7915e4118d040db578221
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-21 17:08:16 +0100

    Commit Exercice 4.1

--- concurrency/src/bin/shared_static.rs
+++ concurrency/src/bin/shared_static.rs
@@ -6,10 +6,12 @@ struct Parameters{
 fn main(){
     let args = Parameters::parse();
     let n=args.n;
+    let stri : &str;
+    stri = "Bonjour";
     let mut tableau = vec![];
     for i in 0..n {
         let thread = std::thread::spawn(move || {
-            println!("Bonjour n°{}", i);
+            println!("{} n°{}", stri,i);
             println!("Au revoir n°{}", i);
         });
         tableau.push(thread);
