177c1eb326e81c28587253279ad5f62d22baffc8
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-29 13:50:50 +0100

    add project port_scanner

---
+++ port_scanner/src/lib.rs
@@ -0,0 +1,7 @@
+use std::net::ToSocketAddrs;
+use std::time::Duration;
+
+pub fn is_open_sync (host: &str, port: u16, timeout:u64) -> bool {
+    let mut addrs_iter = format!("{}:{}", host, port).to_socket_addrs().unwrap();
+    std::net::TcpStream::connect_timeout(&addrs_iter.next().unwrap(), Duration::from_secs(timeout)).is_ok()
+}
