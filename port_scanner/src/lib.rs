3895841f7854c50b8c3c65c4395cf4f7dfcec392
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-11-29 13:53:15 +0100

    add struct Option

--- port_scanner/src/lib.rs
+++ port_scanner/src/lib.rs
@@ -5,3 +5,10 @@ pub fn is_open_sync (host: &str, port: u16, timeout:u64) -> bool {
     let mut addrs_iter = format!("{}:{}", host, port).to_socket_addrs().unwrap();
     std::net::TcpStream::connect_timeout(&addrs_iter.next().unwrap(), Duration::from_secs(timeout)).is_ok()
 }
+
+struct Options{
+    host : String,
+    port_min : u16,
+    port_max : u16,
+    timeout : u64
+}
