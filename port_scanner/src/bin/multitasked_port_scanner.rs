a12f160a153b22616a8ccbe52191e7bae8b1b460
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-01 16:49:25 +0100

    Exercice 3.2 et 3.2.1

--- port_scanner/src/bin/multitasked_port_scanner.rs
+++ port_scanner/src/bin/multitasked_port_scanner.rs
@@ -1,20 +1,27 @@
+use clap::Parser;
 use std::time::Instant;
 use port_scanner::{is_open_async, Options};
-use clap::Parser;
+use port_scanner::is_open_sync;
 
 #[tokio::main]
 async fn main() {
     let instant = Instant::now();
     let options = Options::parse();
-    let host = options.host;
-    let port_min = options.port_min;
-    let port_max = options.port_max;
-    let timeout = options.timeout;
-    for port in port_min..port_max {
-        if is_open_async(&host, port, timeout).await {
-            println!("{} is open", port);
-        }
-    }
+    let mut handles = vec![];
+    for port in options.port_min..options.port_max {
+        let host = options.host.clone();
+        let thread = tokio::spawn(async move  {
+            if is_open_async(&host, port, options.timeout).await {
+                println!("Port {} is open", port);
+            } else {
+                println!("Port {} is closed", port);
+            }
+        });
 
-    println!("Finished in {:?} seconds", instant.elapsed());
+        handles.push(thread);
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
+    println!("Time elapsed: {:?}", instant.elapsed());
 }
