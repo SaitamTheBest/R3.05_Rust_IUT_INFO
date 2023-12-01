a9948e4ee08d1afc9841df1fb226e454a6469b62
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-12-01 16:12:22 +0100

    Exercice 3

--- port_scanner/src/lib.rs
+++ port_scanner/src/lib.rs
@@ -6,6 +6,11 @@ pub fn is_open_sync (host: &str, port: u16, timeout:u64) -> bool {
     let mut addrs_iter = format!("{}:{}", host, port).to_socket_addrs().unwrap();
     std::net::TcpStream::connect_timeout(&addrs_iter.next().unwrap(), Duration::from_secs(timeout)).is_ok()
 }
+
+pub async fn is_open_async(host : &str, port : u16, timeout : u64) ->bool{
+    matches!(tokio::time::timeout(Duration::from_secs(timeout),
+        tokio::net::TcpStream::connect(format!("{}:{}", host, port))).await, Ok(Ok(_)))
+}
 #[derive(Parser)]
 pub struct Options{
     pub host : String,
